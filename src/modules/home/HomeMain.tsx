af6f6d950b41734c8bebe80cec77c5e2a300db4d
Lee JongCheol <whdcjf817@naver.com>
2025-10-19 23:48:01 +0900

    토큰 로컬세션에 저장

--- src/modules/home/HomeMain.tsx
+++ src/modules/home/HomeMain.tsx
@@ -1,4 +1,22 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
 export default function HomeMain() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  useEffect(() => {
+    if (token) {
+      //JWT 토큰을 localStorage에 저장
+      localStorage.setItem("accessToken", token);
+
+      //필요하다면 Zustand에도 사용자 상태 저장하자. (추후 TO-DO)
+      router.replace("/");
+    }
+  }, [token, router]);
+
   return (
     <section className="fixed w-full h-[100%] flex flex-col justify-center mt-20 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center bg-gradient-to-b from-amber-50 to-white">
       <h1 className="text-4xl font-bold mb-4">
